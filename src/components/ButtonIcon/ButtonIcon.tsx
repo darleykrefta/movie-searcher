f8887de1176feca6d6b76f39410b2dde02bb4391
Darley Krefta <darleykrefta@hotmail.com>
2021-03-31 00:19:26 -0300

    feat: added button icon component

---
+++ src/components/ButtonIcon/ButtonIcon.tsx
@@ -0,0 +1,50 @@
+import { ButtonHTMLAttributes, ReactElement } from 'react'
+
+import styled from 'styled-components'
+
+interface ButtonIconProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  leftIcon?: ReactElement
+}
+
+const ButtonIconStyled = styled.button<ButtonIconProps>`
+  cursor: pointer;
+  position: relative;
+
+  transition: all 0.3s;
+
+  font-weight: bold;
+
+  background-color: ${(props) => props.theme.colors.dark};
+  border: none;
+  border-radius: 5px;
+  color: ${(props) => props.theme.colors.lightgrey};
+
+  padding: 12px 12px 12px 12px;
+
+  svg {
+    z-index: 1;
+    width: 24px;
+    height: 24px;
+    transition: all 0.3s;
+  }
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.dark};
+
+    svg path {
+      stroke: ${(props) => props.theme.colors.white};
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+`
+
+export const ButtonIcon = ({ leftIcon, ...props }: ButtonIconProps) => {
+  return (
+    <ButtonIconStyled leftIcon={leftIcon} {...props}>
+      {leftIcon}
+    </ButtonIconStyled>
+  )
+}
