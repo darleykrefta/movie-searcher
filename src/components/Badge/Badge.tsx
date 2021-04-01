194b35e46fcfbd027d5337bf602ea04abe67efa7
Darley Krefta <darleykrefta@hotmail.com>
2021-03-31 21:18:57 -0300

    feat: added badge component

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+import { Box } from 'components/Box'
+import { Text } from 'components/Text'
+
+type BadgeProps = {
+  text: string
+}
+
+const BadgeBodyStyled = styled(Box)`
+  padding: 8px;
+  display: inline-block;
+  border-radius: 5px;
+  background-color: ${(props) => props.theme.colors.midgrey};
+`
+
+const BadgeTextStyled = styled(Text)`
+  font-size: 16px;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.dark};
+`
+
+export const Badge = ({ text }: BadgeProps) => {
+  return (
+    <BadgeBodyStyled>
+      <BadgeTextStyled>{text}</BadgeTextStyled>
+    </BadgeBodyStyled>
+  )
+}
